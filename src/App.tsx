c174cbdc8a5e2a76f645997ae60f723dea65a609
Uqlzsam <161308576+Uqlzsam@users.noreply.github.com>
2025-12-11 13:04:34 +0800

    Done with adding thread background

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import Threads from "./components/Threads";
+
 function App() {
   return (
     <>
-      
+      <section>
+        <div className="w-full h-screen relative bg-black">
+          <Threads amplitude={1} distance={0} enableMouseInteraction={true} />
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
